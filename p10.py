a301686fde3cc24df0d7c45d7bd44bff1fedd6f5
GiGiGan <live10022001@gmail.com>
2017-06-22 16:49:53 +0800

    Add files via upload

---
+++ p10.py
@@ -0,0 +1,22 @@
+cast = ["Cleese", "Palin", 'Jones', "Idle"]
+print(cast)
+
+print(len(cast))
+
+print(cast[1])
+
+cast.append("Gilliam")
+print(cast)
+
+cast.pop()
+
+print(cast)
+
+cast.extend(["Gillam", "Chapman"])
+print(cast)
+
+cast.remove("Chapman")
+print(cast)
+
+cast.insert(0, "Chapman")
+print(cast)
