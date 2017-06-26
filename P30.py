0a064fade707d62487eaab115dcc1cacfd2e1808
GiGiGan <live10022001@gmail.com>
2017-06-26 23:42:11 +0800

    P30 P37 Fix

--- P30.py
+++ P30.py
@@ -4,7 +4,8 @@ movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
 print(movies)
 #["The Holy Grail", 1975, "Terry Jones & Terry Gilliam",
 #91,["Graham Chapman", ["Michael Palin", "John Cleese",
-#"Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+#"Terry Gilliam", "Eric Idle", "Terry Jones"]]] 
+#選擇多行然後 command + / 就可以幫多行加＃
 
 def print_lol(the_list):
     for each_item in the_list:
