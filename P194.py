e360f740134ece59e0ca98b4b139793bce3d9c5a
GiGiGan <live10022001@gmail.com>
2017-08-01 10:45:33 +0800

    P194

---
+++ P194.py
@@ -0,0 +1,12 @@
+class Athlete:
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times
+        
+sarah = Athlete("Sarah Sweeney", "2002-6-17", ["2:58", "2.58", "1.56"])
+james = Athlete("James Jones")
+
+type(sarah)
+
+type(james)
